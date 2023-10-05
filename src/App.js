fa7bf9a12690e38556ada3e52edb9caa3ac2aaeb
Darren013 <143055610+Darren013@users.noreply.github.com>
2023-10-05 20:25:21 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -95,16 +95,17 @@ export default function App() {
         <div className="ButtonContainer">
           <CalcButton label={1} onClick={numberClickHandler} buttonClassName={"CalcButtonNum"} />
           <CalcButton label={2} onClick={numberClickHandler} buttonClassName={"CalcButtonNum"} />
-          <CalcButton label={3} onClick={numberClickHandler} buttonClassName={"CalcButtonNum"} />
-          <CalcButton label={"C"} onClick={clearClickHandler} />
+          <CalcButton label={3} onClick={numberClickHandler} buttonClassName={"CalcButtonNum"} />        
           <CalcButton label={4} onClick={numberClickHandler} buttonClassName={"CalcButtonNum"} />
           <CalcButton label={5} onClick={numberClickHandler} buttonClassName={"CalcButtonNum"} />
-          <CalcButton label={6} onClick={numberClickHandler} buttonClassName={"CalcButtonNum"} />
-          <CalcButton label={"+"} onClick={operatorClickHandler} />
-          <CalcButton label={"="} onClick={equalClickHandler} />
+          <CalcButton label={6} onClick={numberClickHandler} buttonClassName={"CalcButtonNum"} />    
           <CalcButton label={7} onClick={numberClickHandler} buttonClassName={"CalcButtonNum"} />
           <CalcButton label={8} onClick={numberClickHandler} buttonClassName={"CalcButtonNum"} />
           <CalcButton label={9} onClick={numberClickHandler} buttonClassName={"CalcButtonNum"} />
+          <CalcButton label={"C"} onClick={clearClickHandler} />
+          <CalcButton label={"+"} onClick={operatorClickHandler} />
+          <CalcButton label={"-"} onClick={operatorClickHandler} />
+          <CalcButton label={"="} onClick={equalClickHandler} />
           <CalcButton label={"*"} onClick={operatorClickHandler} /> {/* Add multiplication */}
           <CalcButton label={"/"} onClick={operatorClickHandler} /> {/* Add division */}
         </div>
